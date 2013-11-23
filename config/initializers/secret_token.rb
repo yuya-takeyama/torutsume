2dc2c57987b5f44a926dc18fdf344cbd73d194ba
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-11-24 00:56:57 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Torutsume::Application.config.secret_key_base = '5017d79fda259b134b20eff16d1b404145eaa8e6ed6a5364fc60792a7afda86e4d887a49f8ee487a8de54ac08a22cdb8842bc732b3991b78c167a563f6e0aca6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Torutsume::Application.config.session_store :cookie_store, key: '_torutsume_session'
