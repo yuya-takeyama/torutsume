17fafc2313912056b776946eb0b436faff3dd856
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-11-25 00:13:13 +0900

    Add Omniauth config

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook],
          :stretches => 50
 end
