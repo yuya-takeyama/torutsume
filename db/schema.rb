cfe73fa9272d8957f523849acdfa2f8142b28790
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-11-24 23:50:38 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124143727) do
+ActiveRecord::Schema.define(version: 20131124144800) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,9 +26,13 @@ ActiveRecord::Schema.define(version: 20131124143727) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "facebook_id"
+    t.string   "facebook_token"
+    t.string   "facebook_secret"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["facebook_id"], name: "index_users_on_facebook_id", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
