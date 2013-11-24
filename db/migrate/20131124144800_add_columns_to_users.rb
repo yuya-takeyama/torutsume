5d80e43de8bc9d1aed10242d3e787b0a7ab391ce
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-11-24 23:50:14 +0900

    Add index

--- db/migrate/20131124144800_add_columns_to_users.rb
+++ db/migrate/20131124144800_add_columns_to_users.rb
@@ -3,5 +3,7 @@ class AddColumnsToUsers < ActiveRecord::Migration
     add_column :users, :facebook_id, :string
     add_column :users, :facebook_token, :string
     add_column :users, :facebook_secret, :string
+
+    add_index :users, :facebook_id, unique: true
   end
 end
