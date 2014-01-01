2c7e32a9620419488e4f8014f94218d7ba03ba27
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-02 01:16:59 +0900

    Add FactoryGirl helper for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,8 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
+  config.include FactoryGirl::Syntax::Methods
+
   config.before :suite do
     DatabaseRewinder.clean_all
   end
