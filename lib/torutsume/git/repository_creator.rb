f38adb0ad7a06d39ebff63e56c9a804d624d9b2e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-01 20:47:02 +0900

    Implement error handling

--- lib/torutsume/git/repository_creator.rb
+++ lib/torutsume/git/repository_creator.rb
@@ -1,6 +1,9 @@
 module Torutsume
   module Git
+
     class RepositoryCreator
+      attr_reader :error
+
       def initialize(repository_class: repository_class, repository_path_finder: path_finder)
         @repository_path_finder = repository_path_finder
         @repository_class = repository_class
@@ -8,7 +11,12 @@ module Torutsume
 
       def create(text)
         path = @repository_path_finder.find(text)
-        @repository_class.init_at(path, :bare)
+        begin
+          @repository_class.init_at(path, :bare)
+        rescue => e
+          @error = e
+          nil
+        end
       end
     end
   end
--- spec/lib/torutsume/git/repository_creator_spec.rb
+++ spec/lib/torutsume/git/repository_creator_spec.rb
@@ -19,18 +19,40 @@ module Torutsume
       let(:text) { ::Text.new(id: 1) }
 
       describe '#create' do
+        subject { creator.create(text) }
+
         context 'when succeeded' do
           let(:repository_class) do
             repository_class = double('repository_class')
-            allow(repository_class).to receive(:init_at)
+            allow(repository_class).to receive(:init_at).and_return(repository)
             repository_class
           end
 
+          let(:repository) { double(Rugged::Repository) }
+
           it 'should create repository at expected place' do
-            creator.create(text)
+            expect(subject).to be_equal(repository)
             expect(repository_class)
               .to have_received(:init_at)
               .with(repository_path, :bare)
+          end
+        end
+
+        context 'when failed' do
+          let(:repository_class) do
+            repository_class = double('repository_class')
+            allow(repository_class).to receive(:init_at).and_raise(error)
+            repository_class
+          end
+
+          let(:error) { StandardError.new 'Some error occured' }
+
+          it 'should create repository at expected place' do
+            expect(subject).to be_nil
+            expect(repository_class)
+              .to have_received(:init_at)
+              .with(repository_path, :bare)
+            expect(creator.error).to be_equal(error)
           end
         end
       end
