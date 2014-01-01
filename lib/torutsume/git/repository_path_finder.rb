969f099517bf7a964cbf377e155e9004752bf405
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-01 21:20:44 +0900

    Fix to contain Rails.env into repository path

--- lib/torutsume/git/repository_path_finder.rb
+++ lib/torutsume/git/repository_path_finder.rb
@@ -1,12 +1,13 @@
 module Torutsume
   module Git
     class RepositoryPathFinder
-      def initialize(rails_root: rails_root)
+      def initialize(rails_root: _, rails_env: _)
         @rails_root = rails_root
+        @rails_env  = rails_env
       end
 
       def find(text)
-        File.join(@rails_root, 'repo', text.id.to_s)
+        File.join(@rails_root, 'repo', @rails_env, text.id.to_s)
       end
     end
   end
--- spec/lib/torutsume/git/repository_path_finder_spec.rb
+++ spec/lib/torutsume/git/repository_path_finder_spec.rb
@@ -2,12 +2,13 @@ module Torutsume
   module Git
     describe RepositoryPathFinder do
       describe '#find' do
-        subject { RepositoryPathFinder.new(rails_root: rails_root).find(text) }
+        subject { RepositoryPathFinder.new(rails_root: rails_root, rails_env: rails_env).find(text) }
         let(:rails_root) { '/root' }
+        let(:rails_env) { 'test' }
         let(:text) { ::Text.new(id: 1) }
 
         it 'should return path for git repository' do
-          expect(subject).to eq('/root/repo/1')
+          expect(subject).to eq('/root/repo/test/1')
         end
       end
     end
