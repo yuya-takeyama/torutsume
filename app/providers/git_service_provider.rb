038ee490f0c6bc5efffa3bd78d5c5c8021a6e755
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-01 21:32:12 +0900

    Define git.repository_creator and git.repository_path_finder

--- app/providers/git_service_provider.rb
+++ app/providers/git_service_provider.rb
@@ -1,5 +1,17 @@
 class GitServiceProvider < Dee::Rails::ServiceProvider
   provide do
-    # Your service provider definition goes here
+    singleton 'git.repository_creator' do
+      Torutsume::Git::RepositoryCreator.new(
+        repository_class: Rugged::Repository,
+        repository_path_finder: self['git.repository_path_finder'],
+      )
+    end
+
+    singleton 'git.repository_path_finder' do
+      Torutsume::Git::RepositoryPathFinder.new(
+        rails_root: Rails.root,
+        rails_env: Rails.env,
+      )
+    end
   end
 end
