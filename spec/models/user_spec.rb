63b7dd9a12439a640a2162059768e1707aba8c1c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-05 01:46:53 +0900

    rake db:migrate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
-require 'spec_helper'
-
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name_for_git' do
+    subject { user.name_for_git }
+
+    context 'when name is present' do
+      let(:user) { User.new name: name }
+      let(:name) { 'Test User' }
+
+      it 'should be its name' do
+        expect(subject).to eq(name)
+      end
+    end
+  end
 end
