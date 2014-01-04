98120c8de22a5a504c3f4462509a0756ce5cd876
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-01-05 03:31:29 +0900

    Fix user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user, :class => User do
-    id { 1000 }
-    email { 'testuser@example.com' }
-    password { 'password' }
+    id 1000
+    username 'testuser'
+    email 'testuser@example.com'
+    password 'password'
   end
 end
